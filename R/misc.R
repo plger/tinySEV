508e801807266cead0109e0f4ecb0ee3ce62fc45
plger <pl.germain@gmail.com>
2024-11-08 10:00:45 +0100

    updated homogenization

--- R/misc.R
+++ R/misc.R
@@ -80,7 +80,10 @@ grepGene <- function(x, g, ignore.case=TRUE){
   if(!is.data.frame(x)) return(NULL)
   colnames(x) <- gsub("log2FoldChange|log2Fold|log2FC|log2\\(fold_change\\)|log2\\.fold_change\\.",
                       "logFC", colnames(x))
-  
+  if(!any(colnames(x)=="logFC") & (length(w <- grep("logFC",colnames(x)))>0)){
+    x$logFC <- rowMeans(as.matrix(x[,w,drop=FALSE]))
+  }
+
   abf <- head(intersect(colnames(x),
                         c("logCPM", "meanExpr", "AveExpr", "baseMean")), 1)
   if (length(abf)==1){
@@ -90,8 +93,8 @@ grepGene <- function(x, g, ignore.case=TRUE){
     x$meanExpr <- log(1+x$value_1+x$value_2)
   }
   colnames(x) <- gsub("P\\.Value|pvalue|p_value|pval", "PValue", colnames(x))
-  colnames(x) <- gsub("padj|adj\\.P\\.Val|q_value|qval", "FDR", colnames(x))
-  if (!("FDR" %in% colnames(x))) 
+  colnames(x) <- gsub("padj|adj\\.P\\.Val|q_value|qval|p_adj.loc|ihw", "FDR", colnames(x))
+  if (!("FDR" %in% colnames(x)))
     x$FDR <- p.adjust(x$PValue, method = "fdr")
   f <- grep("^logFC$",colnames(x),value=TRUE)
   if(length(f)==0) f <- grep("logFC",colnames(x),value=TRUE)
